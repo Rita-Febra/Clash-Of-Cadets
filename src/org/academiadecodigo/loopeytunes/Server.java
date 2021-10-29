b93106c4965ef9d32c546f2fb657fd2850ddf96e
João Amaral <java.amaral@gmail.com>
2021-10-29 18:06:09 +0100

    Basic structure

---
+++ src/org/academiadecodigo/loopeytunes/Server.java
@@ -0,0 +1,19 @@
+package org.academiadecodigo.loopeytunes;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private int serverPort;
+    
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+}
