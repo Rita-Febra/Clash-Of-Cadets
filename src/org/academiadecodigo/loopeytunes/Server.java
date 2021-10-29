db826f9752066c00e9d8560be0bfa72835460d9b
João Amaral <java.amaral@gmail.com>
2021-10-29 19:49:23 +0100

    Server with max players

--- src/org/academiadecodigo/loopeytunes/Server.java
+++ src/org/academiadecodigo/loopeytunes/Server.java
@@ -1,19 +1,55 @@
 package org.academiadecodigo.loopeytunes;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
     private ServerSocket serverSocket;
-    private int serverPort;
-    
+    private final int serverPort = 6666;
+    private Socket[] sockets = new Socket[2];
+    private int numConnect = 0;
+    private Socket clientSocket;
 
-    public static void main(String[] args) {
+    public Server() {
+
+        try {
+            System.out.println("Binding to port: " + serverPort);
+            serverSocket = new ServerSocket(serverPort);
+
+            while (!serverSocket.isClosed()) {
+
+                System.out.println("Waiting for player...");
+                clientSocket = serverSocket.accept();
+
+                if (numConnect < 2) {
+
+                    sockets[numConnect] = clientSocket;
+                    numConnect++;
+                } else {
+                    PrintWriter fullServer = new PrintWriter(clientSocket.getOutputStream(), true);
+                    fullServer.println("Server is full. Please try later");
+                    fullServer.flush();
+                    clientSocket.close();
+
+                }
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
+    public static void main(String[] args) {
 
+        Server server = new Server();
 
+    }
 
 
 }
